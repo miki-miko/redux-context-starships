2c82ad573a4c5e198acd666659e2db14a128d5a9
F-Moody <federico.cirasa@gmail.com>
2021-03-23 11:43:32 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import {Button} from 'react-bootstrap'
 import './App.css';
+import SimpleCard from "./SimpleCard";
+import StarShipForm from "./StarShipForm";
+axios.defaults.headers.post['Content-Type'] = 'application/json'
+
 
 function App() {
+  const [starships, setStarships] = useState([])
+  const [showFormModal, setShowFormModal] = useState(false)
+  const [newStarship, setNewStarship] = useState({})
+
+  const getAllStarShips = async () => {
+    const { data: starships } = await axios.get('http://localhost:3000/starships')
+    setStarships(starships)
+  }
+
+  useEffect(() => {
+
+    getAllStarShips()
+  },[])
+
+  const checkForm = () => newStarship.name && newStarship.manufacturer && newStarship.image && newStarship.crew >= 0 && newStarship.passengers >= 0 && newStarship.cargo_capacity >= 0
+
+  const onInputChange = (e) => {
+    setNewStarship(starship => ({...starship, [e.target.name]: e.target.value }))
+  }
+
+  const closeForm = () => {
+    setNewStarship({})
+    setShowFormModal(false)
+  }
+
+  const addNewStarShip = async e => {
+    e.preventDefault()
+    console.log(newStarship, 'newstarship')
+    if(checkForm()) {
+      await axios.post('http://localhost:3000/starships', newStarship)
+      await getAllStarShips()
+    } else {
+      throw new Error ('Form not Valid')
+    }
+  }
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className={"App"}>
+        <div className={"PostsContainer"}>{starships.map(starship => (<SimpleCard fetchAll={getAllStarShips} starship={starship} key={starship.id}/>))}</div>
+        <StarShipForm starship={newStarship} show={showFormModal} handleClose={closeForm} onInputChange={onInputChange} addPost={addNewStarShip}/>
+        <Button className={'addButton'} size={'lg'} variant={'primary'} onClick={() => setShowFormModal(true)}> + </Button>
+      </div>
   );
 }
 
