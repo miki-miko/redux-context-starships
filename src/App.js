bdd6ab7ab05cd2361eade0779a10ae34d60759b6
Michelangelo Morabito <michimorabito@gmail.com>
2021-04-20 15:29:05 +0200

    fix: refactoring and adding Bootstrap to package.json

--- src/App.js
+++ src/App.js
@@ -1,55 +1,78 @@
-import axios from 'axios'
-import {useContext, useEffect, useState} from 'react'
-import {Button} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import SimpleCard from "./SimpleCard";
-import StarShipForm from "./StarShipForm";
-import {useDispatch, useSelector} from "react-redux";
-import {addShip, fetchAllShips} from "./store/actions";
-axios.defaults.headers.post['Content-Type'] = 'application/json'
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Button } from 'react-bootstrap';
+import SimpleCard from './components/SimpleCard';
+import StarShipForm from './components/StarShipForm';
+import { useDispatch, useSelector } from 'react-redux';
+import { addShip, fetchAllShips } from './store/actions';
 
+axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 function App() {
-  const [showFormModal, setShowFormModal] = useState(false)
-  const [newStarship, setNewStarship] = useState({})
-  const dispatch = useDispatch()
-  const starships = useSelector(state => state.starships)
-
-
+  const [showFormModal, setShowFormModal] = useState(false);
+  const [newStarship, setNewStarship] = useState({});
+  const dispatch = useDispatch();
+  const starships = useSelector((state) => state.starships);
 
   useEffect(() => {
-    dispatch(fetchAllShips())
-  },[])
+    dispatch(fetchAllShips());
+  }, []);
 
-  const checkForm = () => newStarship.name && newStarship.manufacturer && newStarship.image && newStarship.crew >= 0 && newStarship.passengers >= 0 && newStarship.cargo_capacity >= 0
+  const checkForm = () =>
+    newStarship.name &&
+    newStarship.manufacturer &&
+    newStarship.image &&
+    newStarship.crew >= 0 &&
+    newStarship.passengers >= 0 &&
+    newStarship.cargo_capacity >= 0;
 
   const onInputChange = (e) => {
-    setNewStarship(starship => ({...starship, [e.target.name]: e.target.value }))
-  }
+    setNewStarship((starship) => ({
+      ...starship,
+      [e.target.name]: e.target.value,
+    }));
+  };
 
   const closeForm = () => {
-    setNewStarship({})
-    setShowFormModal(false)
-  }
+    setNewStarship({});
+    setShowFormModal(false);
+  };
 
-  const addNewStarShip = async e => {
-    e.preventDefault()
-    if(checkForm()) {
-      dispatch(addShip(newStarship))
+  const addNewStarShip = async (e) => {
+    e.preventDefault();
+    if (checkForm()) {
+      dispatch(addShip(newStarship));
     } else {
-      throw new Error ('Form not Valid')
+      throw new Error('Form not Valid');
     }
-  }
-
-
-
+  };
 
   return (
-      <div className={"App"}>
-        <div className={"PostsContainer"}>{starships.map(starship => (<SimpleCard starship={starship} key={starship.id}/>))}</div>
-        <StarShipForm starship={newStarship} show={showFormModal} handleClose={closeForm} onInputChange={onInputChange} addPost={addNewStarShip}/>
-        <Button className={'addButton'} size={'lg'} variant={'primary'} onClick={() => setShowFormModal(true)}> + </Button>
+    <div className={'App'}>
+      <div className={'PostsContainer'}>
+        {starships.map((starship) => (
+          <SimpleCard starship={starship} key={starship.id} />
+        ))}
       </div>
+      <StarShipForm
+        starship={newStarship}
+        show={showFormModal}
+        handleClose={closeForm}
+        onInputChange={onInputChange}
+        addPost={addNewStarShip}
+      />
+      <Button
+        className={'addButton'}
+        size={'lg'}
+        variant={'primary'}
+        onClick={() => setShowFormModal(true)}
+      >
+        {' '}
+        +{' '}
+      </Button>
+    </div>
   );
 }
 
